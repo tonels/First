d8b8cf4b2d2f37d3e6489e81a74391a512873da4
liangshuai <1589900136@qq.com>
2018-11-12 22:07:06 +0800

    vo

---
+++ src/main/java/com/aust/first/vo/StuVo.java
@@ -0,0 +1,23 @@
+package com.aust.first.vo;
+
+public class StuVo {
+
+	private String sname;
+	
+	private Integer totalGrade;
+	
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public Integer getTotalGrade() {
+		return totalGrade;
+	}
+	public void setTotalGrade(Integer totalGrade) {
+		this.totalGrade = totalGrade;
+	}
+	
+	
+}

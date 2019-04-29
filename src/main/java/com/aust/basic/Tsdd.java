4621e29700f880057f2e84917ffe5e4b0d5cb976
liangshuai <44797618+tonels@users.noreply.github.com>
2019-04-29 21:23:42 +0800

    Update Tsdd.java

--- src/main/java/com/aust/basic/Tsdd.java
+++ src/main/java/com/aust/basic/Tsdd.java
@@ -3,7 +3,7 @@ package com.aust.basic;
 public class Tsdd {
 
     public static void main(String[] args) {
-        System.out.print("手动添加");
+        System.out.print("手动的身份士大夫添加");
         System.out.print("GIT测试");
     }
 }

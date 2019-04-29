5d65de752259bb21c45e011d7208e802bcf9f6f5
liangshuai <1589900136@qq.com>
2019-04-29 21:24:29 +0800

    liangshuai_04292018

--- src/main/java/com/aust/basic/Tsdd.java
+++ src/main/java/com/aust/basic/Tsdd.java
@@ -4,6 +4,7 @@ public class Tsdd {
 
     public static void main(String[] args) {
         System.out.print("手动添加");
+        System.out.print("手动添sdsdfsdfs加");
         System.out.print("GIT测试");
     }
 }

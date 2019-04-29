b5c4d9f6b0fda8bd92893f3dc184e6dc03a1a405
liangshuai <44797618+tonels@users.noreply.github.com>
2019-04-29 21:37:04 +0800

    Update Tsdd.java

--- src/main/java/com/aust/basic/Tsdd.java
+++ src/main/java/com/aust/basic/Tsdd.java
@@ -3,7 +3,7 @@ package com.aust.basic;
 public class Tsdd {
 
     public static void main(String[] args) {
-        System.out.print("手动添加大撒大撒发射点发生");
+        System.out.print("手动添加大撒大撒发射发士大夫电风扇点发生");
 
 
 

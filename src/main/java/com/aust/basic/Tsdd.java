411e6a6933efa8c9e819ca8296bb5abaa202ce7c
liangshuai <1589900136@qq.com>
2019-04-30 15:52:44 +0800

    提交更改

--- src/main/java/com/aust/basic/Tsdd.java
+++ src/main/java/com/aust/basic/Tsdd.java
@@ -1,14 +1,11 @@
-package com.aust.basic;
+﻿package com.aust.basic;
 
 public class Tsdd {
 
     public static void main(String[] args) {
-        System.out.print("手动添dsf加大");
-        System.out.print("手发fgfd");
-        System.out.print("GIT测试");
-        System.out.print("GIT测试");
-
-
-
+        System.out.print("手动添dsdfdsfsaf加大");
+        System.out.print("手发dsdsaadfgfd");
+        System.out.print("GITdasdsafsdf测fsdf试");
+        System.out.print("GIT测dasd试");
     }
 }

587e9c2ff2ab69b84441cb1cafafcf0dd24788db
liangshuai <44797618+tonels@users.noreply.github.com>
2019-04-30 10:31:43 +0800

    Update Tsdd.java

--- src/main/java/com/aust/basic/Tsdd.java
+++ src/main/java/com/aust/basic/Tsdd.java
@@ -3,11 +3,8 @@ package com.aust.basic;
 public class Tsdd {
 
     public static void main(String[] args) {
-        System.out.print("手动添加大撒大撒的沙发电视柜发射发士大夫电风扇点发生");
-
-
-
-
+        System.out.print("手动添加大");
+        System.out.print("手发fgfd");
         System.out.print("GIT测试");
     }
 }

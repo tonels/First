39aa92f03a191f71013b0dd8160d39666383ce42
liangshuai <44797618+tonels@users.noreply.github.com>
2019-04-30 15:42:41 +0800

    Update Tsdd.java

--- src/main/java/com/aust/basic/Tsdd.java
+++ src/main/java/com/aust/basic/Tsdd.java
@@ -3,14 +3,6 @@ package com.aust.basic;
 public class Tsdd {
 
     public static void main(String[] args) {
-        System.out.print("手动添dsf加大");
-        System.out.print("手发fgfd");
-       //sdsds
-        System.out.print("GIT测试");
-        System.out.print("GIT测试");
-        System.out.print("手动添dsf加大");
-        System.out.print("手发fgfd");
-       
         System.out.print("GIT测试");
         System.out.print("GIT测试");
 

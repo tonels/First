621bf62ab4e634d8c91b54035eeebbb1f683fc12
liangshuai <44797618+tonels@users.noreply.github.com>
2019-04-30 15:37:11 +0800

    sds

--- src/main/java/com/aust/basic/Tsdd.java
+++ src/main/java/com/aust/basic/Tsdd.java
@@ -5,8 +5,12 @@ public class Tsdd {
     public static void main(String[] args) {
         System.out.print("手动添dsf加大");
         System.out.print("手发fgfd");
-        System.out.print("手动添加大");
+       //sdsds
+        System.out.print("GIT测试");
+        System.out.print("GIT测试");
+        System.out.print("手动添dsf加大");
         System.out.print("手发fgfd");
+       
         System.out.print("GIT测试");
         System.out.print("GIT测试");
 

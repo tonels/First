03f4c2f9c94bf32f5eeb123eedb65dee1c86322d
liangshuai <1589900136@qq.com>
2019-04-30 15:58:38 +0800

    手动添加3

--- src/main/java/com/aust/basic/Tsdd.java
+++ src/main/java/com/aust/basic/Tsdd.java
@@ -5,6 +5,10 @@ public class Tsdd {
     public static void main(String[] args) {
         System.out.print("GIT测试");
         System.out.print("GIT测试");
+        System.out.print("GIT测试");
+        System.out.print("GIT测试");
+        System.out.print("GIT测试");
+        System.out.print("GIT测试");
 
 
     }

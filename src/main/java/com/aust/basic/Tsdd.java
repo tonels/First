3f1a1b9314ea4a9287f718a4896135d72e2c4bfd
liangshuai <1589900136@qq.com>
2019-04-30 15:53:48 +0800

    手动添加1

--- src/main/java/com/aust/basic/Tsdd.java
+++ src/main/java/com/aust/basic/Tsdd.java
@@ -3,9 +3,10 @@
 public class Tsdd {
 
     public static void main(String[] args) {
-        System.out.print("手动添dsdfdsfsaf加大");
-        System.out.print("手发dsdsaadfgfd");
-        System.out.print("GITdasdsafsdf测fsdf试");
-        System.out.print("GIT测dasd试");
+        System.out.print("手动添加1");
+        System.out.print("手动添加1");
+        System.out.print("手动添加1");
+        System.out.print("手动添加1");
+        System.out.print("手动添加1");
     }
 }

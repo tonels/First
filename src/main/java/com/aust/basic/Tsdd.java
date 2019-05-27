bbcf13d5e0528bf2862b2c601d24b56433918754
liangshuai <1589900136@qq.com>
2019-05-27 19:34:22 +0800

    s

--- src/main/java/com/aust/basic/Tsdd.java
+++ src/main/java/com/aust/basic/Tsdd.java
@@ -4,10 +4,6 @@ public class Tsdd {
 
     public static void main(String[] args) {
         System.out.print("GIT测试");
-        System.out.print("GIT测试");
-        System.out.print("GIT测试");
-        System.out.print("GIT测试");
-        System.out.print("GIT测试");
-        System.out.print("GIT房贷首付测试");
+
     }
 }
